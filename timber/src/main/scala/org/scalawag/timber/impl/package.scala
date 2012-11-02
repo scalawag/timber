7220272c12108443741196654382eab74f937462
Justin Patterson <justin@scalawag.org>
2012-11-01 20:13:31 -0500

    Make the DefaultConfiguration use AutoFlush to prevent lost messages

--- timber/src/main/scala/org/scalawag/timber/impl/package.scala
+++ timber/src/main/scala/org/scalawag/timber/impl/package.scala
@@ -1,14 +1,13 @@
 package org.scalawag.timber
 
 import impl.dispatcher.Configuration
-//import impl.dispatcher.Configuration._
 import impl.formatter.DefaultEntryFormatter
-import impl.receiver.{Asynchronous, OutputStreamReceiver}
+import impl.receiver.{AutoFlush, StderrReceiver, Asynchronous}
 
 package object impl {
 
   // This needs to be moved to another package (so it doesn't get initialized with the rest of the stuff in here)
-  lazy val DefaultConfiguration: Configuration = new ImmutableReceiver(Asynchronous(new OutputStreamReceiver(new DefaultEntryFormatter,System.err)))
+  lazy val DefaultConfiguration: Configuration = new ImmutableReceiver(Asynchronous(new StderrReceiver(new DefaultEntryFormatter) with AutoFlush))
 
 }
 
