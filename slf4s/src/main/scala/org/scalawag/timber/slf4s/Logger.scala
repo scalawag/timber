097453b768564c765bb85941a58e573953565f45
Justin Patterson <justin@scalawag.org>
2013-06-16 17:11:10 -0500

    Making Logger concrete

--- slf4s/src/main/scala/org/scalawag/timber/slf4s/Logger.scala
+++ slf4s/src/main/scala/org/scalawag/timber/slf4s/Logger.scala
@@ -1,5 +1,7 @@
 package org.scalawag.timber.slf4s
 
+import org.scalawag.timber.slf4s.impl.{EntryDispatcher, Entry}
+
 /** The main application interface into timber.
   *
   * Loggers are what applications use to inject @link Entries into the system.
@@ -12,13 +14,11 @@ package org.scalawag.timber.slf4s
   *
   * Arguably, this is better for the application code because your application code shouldn't be dependent on the
   * logging configuration.  You have to try really hard to write code that depends on logging configuration.
+  *
+  * @param name the value that this Logger uses as the "logger" field in the Entries that it generates.
   */
 
-trait Logger {
-
-  /** The value that this Logger uses as the "logger" field in the Entries that it generates. */
-
-  val name:String
+class Logger(val name:String,val dispatcher:EntryDispatcher) {
 
   /** Submits an entry to the logging system.
     *
@@ -27,11 +27,23 @@ trait Logger {
     * @param tags the (optional) set of tags to include with the entry
     */
 
-  def log(level:Level,message:Message,tags:Set[Tag])
+  def log(level:Level,message:Message,tags:Set[Tag]) {
+    dispatcher.dispatch(buildEntry(level,message,tags))
+  }
 
   def log(level:Level,message:Message):Unit = log(level,message,Set.empty[Tag])
   def log(level:Level,message:Message,tag:Tag*):Unit = log(level,message,tag.toSet)
   def log(level:Level,tag:Tag*)(message:Message):Unit = log(level,message,tag.toSet)
+
+  protected def buildEntry(level:Level,message:Message,tags:Set[Tag]) =
+    Entry(message = message,
+          logger = this.name,
+          level = level,
+          timestamp = System.currentTimeMillis,
+          thread = Thread.currentThread,
+          tags = tags,
+          context = LoggingContext.get)
+
 }
 
 /* timber -- Copyright 2012 Justin Patterson -- All Rights Reserved */
---
+++ slf4s/src/main/scala/org/scalawag/timber/slf4s/impl/EntryDispatcher.scala
@@ -0,0 +1,5 @@
+package org.scalawag.timber.slf4s.impl
+
+trait EntryDispatcher {
+  def dispatch(entry:Entry)
+}
