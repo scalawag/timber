855afda093de81bf14d15b51bee45826bd42420e
Justin Patterson <justin@scalawag.org>
2016-01-03 16:01:39 -0600

    Add MessageOnlyEntryFormatter

---
+++ timber/src/main/scala/org/scalawag/timber/backend/receiver/formatter/MessageOnlyEntryFormatter.scala
@@ -0,0 +1,7 @@
+package org.scalawag.timber.backend.receiver.formatter
+
+import org.scalawag.timber.api.Entry
+
+object MessageOnlyEntryFormatter extends EntryFormatter {
+  override def format(value:Entry) = value.message.map(_.text).getOrElse("")
+}
